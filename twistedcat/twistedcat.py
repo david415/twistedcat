fd5486549dae40de44c224abb07a21be1ec73302
David Stainton <dstainton415@gmail.com>
2014-06-21 23:07:57 +0000

    Add EndpointCrossOver class to encapsulate logic
    Big code clean-up!
    This commit simplified the commandline arguments...
    making them all position.

--- twistedcat/twistedcat.py
+++ twistedcat/twistedcat.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from twisted.internet.endpoints import clientFromString, serverFromString
-from twisted.internet import reactor, protocol
+from twisted.internet import reactor, protocol, defer
+from twisted.internet.interfaces import IStreamClientEndpoint, IStreamServerEndpoint
 from twisted.protocols.portforward import Proxy, ProxyClient
 import argparse
 import sys
+
 
 class ProxyEndpointProtocol(Proxy):
 
@@ -20,15 +22,22 @@ class ProxyEndpointProtocol(Proxy):
             self.peer.transport.resumeProducing()
 
     def connectionLost(self, reason):
+        # XXX
+        #self.peer.transport.loseConnection()
         self.transport.loseConnection()
+
+        if self.factory.handleLostConnection is not None:
+            self.factory.handleLostConnection()
+
 
 class ProxyEndpointProtocolFactory(protocol.Factory):
 
     protocol = ProxyEndpointProtocol
 
-    def __init__(self):
+    def __init__(self, handleLostConnection=None):
         self.peerFactory = None
         self.protocolInstance = None
+        self.handleLostConnection = handleLostConnection
 
     def setPeerFactory(self, peerFactory):
         self.peerFactory = peerFactory
@@ -42,13 +51,59 @@ class ProxyEndpointProtocolFactory(protocol.Factory):
         return self.protocolInstance
 
 
+def maybeClientFromString(reactor, endpointDescriptor):
+    try:
+        endpoint = clientFromString(reactor, endpointDescriptor)
+    except TypeError, e:
+        endpoint = None
+    return endpoint
 
-class EndpointCombinator(object):
+def maybeServerFromString(reactor, endpointDescriptor):
+    try:
+        endpoint = serverFromString(reactor, endpointDescriptor)
+    except TypeError, e:
+        endpoint = None
+    return endpoint
 
-    def __init__(self, endpoint1, endpoint2):
-        pass
+class InvalidEndpointDescriptorError(Exception):
+    """We cannot make a connection without valid endpoint descriptors"""
+
+def endpointFromString(reactor, endpointDescriptor):
+    endpoint = maybeClientFromString(reactor, endpointDescriptor)
+    if endpoint is None:
+        endpoint = maybeServerFromString(reactor, endpointDescriptor)
+    if endpoint is None:
+        raise InvalidEndpointDescriptorError()
+    return endpoint
 
 
+class EndpointCrossOver(object):
+
+    def __init__(self, endpoint1, endpoint2, handleError=None):
+        self.endpoint1 = endpoint1
+        self.endpoint2 = endpoint2
+        self.handleError = handleError
+
+    def appendListeningPort(self, port):
+        self.listeningPorts.append(port)
+
+    def _openEndpoint(self, endpoint, factory):
+        if IStreamClientEndpoint.providedBy(endpoint):
+            d = endpoint.connect(factory)
+        elif IStreamServerEndpoint.providedBy(endpoint):
+            d = endpoint.listen(factory)
+        else:
+            raise ValueError('must provide either IStreamClientEndpoint or IStreamServerEndpoint')
+
+    def join(self):
+        self.factory1 = ProxyEndpointProtocolFactory(handleLostConnection=self.handleError)
+        self.factory2 = ProxyEndpointProtocolFactory(handleLostConnection=self.handleError)
+
+        self.factory1.setPeerFactory(self.factory2)
+        self.factory2.setPeerFactory(self.factory1)
+
+        self._openEndpoint(self.endpoint1, self.factory1)
+        self._openEndpoint(self.endpoint2, self.factory2)
 
 
 # twistedcat - twisted endpoint concatenator
@@ -60,67 +115,21 @@ def main():
     num_servers = 0
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--server', type=str, action="append", nargs='+', dest='server', help='server endpoint accumulator')
-    parser.add_argument('client', action="append", nargs='*')
+    parser.add_argument('endpoints', action="append", nargs='*')
     args = parser.parse_args()
 
-    if args.server is not None:
-        num_servers += len(args.server)
-        servers = map(lambda x:x[0], args.server)
-
-    if num_servers + len(args.client[0]) != 2:
+    if len(args.endpoints[0]) != 2:
         parser.print_help()
         sys.exit(1)
 
-    if len(args.client[0]) != 0:
-        clients = args.client[0]
+    def handle_error():
+        if reactor.running:
+            reactor.stop()
 
-    if servers is not None and clients is not None:
-        serverEndpointDescriptor = servers[0]
-        clientEndpointDescriptor = clients[0]
-
-        serverEndpoint = serverFromString(reactor, serverEndpointDescriptor)
-        clientEndpoint = clientFromString(reactor, clientEndpointDescriptor)
-
-        clientEndpointFactory = ProxyEndpointProtocolFactory()
-        serverEndpointFactory = ProxyEndpointProtocolFactory()
-
-        clientEndpointFactory.setPeerFactory(serverEndpointFactory)
-        serverEndpointFactory.setPeerFactory(clientEndpointFactory)
-
-        connectDeferred = clientEndpoint.connect(clientEndpointFactory)
-        listeningPortDeferred = serverEndpoint.listen(serverEndpointFactory)
-    elif servers is None and clients is not None:
-        clientEndpointDescriptor1 = clients[0]
-        clientEndpointDescriptor2 = clients[1]
-
-        clientEndpoint1 = clientFromString(reactor, clientEndpointDescriptor1)
-        clientEndpoint2 = clientFromString(reactor, clientEndpointDescriptor2)
-
-        clientEndpointFactory1 = ProxyEndpointProtocolFactory()
-        clientEndpointFactory2 = ProxyEndpointProtocolFactory()
-
-        clientEndpointFactory1.setPeerFactory(clientEndpointFactory2)
-        clientEndpointFactory2.setPeerFactory(clientEndpointFactory1)
-
-        clientEndpoint1.connect(clientEndpointFactory1)
-        clientEndpoint2.connect(clientEndpointFactory2)
-    else:
-        serverEndpointDescriptor1 = servers[0]
-        serverEndpointDescriptor2 = servers[1]
-
-        serverEndpoint1 = serverFromString(reactor, serverEndpointDescriptor1)
-        serverEndpoint2 = serverFromString(reactor, serverEndpointDescriptor2)
-
-        serverEndpointFactory1 = ProxyEndpointProtocolFactory()
-        serverEndpointFactory2 = ProxyEndpointProtocolFactory()
-
-        serverEndpointFactory1.setPeerFactory(serverEndpointFactory2)
-        serverEndpointFactory2.setPeerFactory(serverEndpointFactory1)
-
-        listeningPortDeferred1 = serverEndpoint1.listen(serverEndpointFactory1)
-        listeningPortDeferred2 = serverEndpoint2.listen(serverEndpointFactory2)
-
+    endpoint_pipe = EndpointCrossOver(endpointFromString(reactor, args.endpoints[0][0]),
+                                      endpointFromString(reactor, args.endpoints[0][1]),
+                                      handleError=handle_error)
+    endpoint_pipe.join()
     reactor.run()
 
 if __name__ == '__main__':
