5d48e05180a53c265ecd59f2890b066b2ac25237
Leif Ryge <leif@synthesize.us>
2015-10-20 18:54:26 +0000

    quick hack to make this a tor control port filter
    ...allowing arm (the Anonymous Relay Monitor) to register for bandwidth events
    and nothing else.

--- twistedcat/proxy.py
+++ twistedcat/proxy.py
@@ -1,10 +1,70 @@
-
+from twisted.protocols import basic
 from twisted.internet import protocol
-from twisted.protocols.portforward import Proxy, ProxyClient
 from twisted.internet.interfaces import IStreamClientEndpoint, IStreamServerEndpoint
 
+ALLOWED = """
+PROTOCOLINFO
+250-PROTOCOLINFO 1
+250-AUTH METHODS=COOKIE,SAFECOOKIE COOKIEFILE="/var/run/tor/control.authcookie"
+250-VERSION Tor="0.2.6.10"
+250 OK
+""".strip().split("\n")
 
-class ProxyEndpointProtocol(Proxy):
+ALLOWED_PREFIXES = """
+650 BW
+AUTHENTICATE
+GETCONF BandwidthRate
+250 BandwidthRate=
+GETCONF BandwidthBurst
+250 BandwidthBurst=
+GETINFO traffic/read
+250-traffic/read=
+GETINFO traffic/written
+250-traffic/written=
+GETCONF ControlPort
+250 ControlPort=9951
+""".strip().split("\n")
+
+REPLACEMENTS = {
+    "SETEVENTS NOTICE ERR NEWDESC NEWCONSENSUS WARN CIRC BW NS":
+        "SETEVENTS NEWCONSENSUS BW"
+}
+
+FILTER = True
+
+class LineProxyEndpointProtocol(basic.LineReceiver):
+    noisy = True
+    peer = None
+    label = None
+
+    def setPeer(self, peer):
+        self.peer = peer
+
+    def lineReceived(self, line):
+        allow = False
+        if FILTER == False:
+            print "%s: %r" % (self.label, line)
+            self.peer.transport.write(line + self.delimiter)
+            return
+        if line in REPLACEMENTS:
+            print "%s replacing %r with %r" % (self.label, line, REPLACEMENTS[line])
+            line = REPLACEMENTS[line]
+            allow = True
+        elif line in ALLOWED:
+            allow = True
+        else:
+            for prefix in ALLOWED_PREFIXES:
+                if line.startswith(prefix):
+                    allow = True
+                    break
+        if allow:
+            print "%s allowed: %r" % (self.label, line,)
+            self.peer.transport.write(line + self.delimiter)
+        else:
+            print "%s filtered: %r" % (self.label, line,)
+            if self.label == "A":
+                print "sending 510"
+                self.sendLine("510 Command filtered")
 
     def connectionMade(self):
         if self.factory.peerFactory.protocolInstance is None:
@@ -27,18 +87,20 @@ class ProxyEndpointProtocol(Proxy):
 
 class ProxyEndpointProtocolFactory(protocol.Factory):
 
-    protocol = ProxyEndpointProtocol
+    protocol = LineProxyEndpointProtocol
 
-    def __init__(self, handleLostConnection=None):
+    def __init__(self, handleLostConnection=None, label=None):
         self.peerFactory = None
         self.protocolInstance = None
         self.handleLostConnection = handleLostConnection
+        self.label = label
 
     def setPeerFactory(self, peerFactory):
         self.peerFactory = peerFactory
 
     def buildProtocol(self, *args, **kw):
         self.protocolInstance = protocol.Factory.buildProtocol(self, *args, **kw)
+        self.protocolInstance.label = self.label
 
         if self.peerFactory.protocolInstance is not None:
             self.protocolInstance.setPeer(self.peerFactory.protocolInstance)
@@ -63,8 +125,8 @@ class EndpointCrossOver(object):
             raise ValueError('must provide either IStreamClientEndpoint or IStreamServerEndpoint')
 
     def join(self):
-        self.factory1 = ProxyEndpointProtocolFactory(handleLostConnection=self.handleError)
-        self.factory2 = ProxyEndpointProtocolFactory(handleLostConnection=self.handleError)
+        self.factory1 = ProxyEndpointProtocolFactory(handleLostConnection=self.handleError, label="A")
+        self.factory2 = ProxyEndpointProtocolFactory(handleLostConnection=self.handleError, label="B")
 
         self.factory1.setPeerFactory(self.factory2)
         self.factory2.setPeerFactory(self.factory1)
